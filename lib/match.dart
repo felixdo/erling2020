cccdb27f6843cde74c85b2df71cd8519dd71f15a
Felix Dorner <felix.dorner@gmail.com>
2020-03-02 15:12:13 +0100

    List matches by spieltag, if no spieltag, list todays matches
    Signed-off-by: Felix Dorner <felix.dorner@gmail.com>

--- lib/match.dart
+++ lib/match.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/cupertino.dart';
 
-import './score.dart';
 
 class Match {
   String homeTeam, awayTeam;
+  Map<String, dynamic> score;
 
-  Match(this.homeTeam, this.awayTeam);
-  //factory Match.fromJSON(Map<dynamic, dynamic> match) => Match(homeTeam: match["homeTeam"]["name"], awayTeam: match["awayTeam"]["name"], score: Score.fromJSON(match["score"]));
+  Match(this.homeTeam, this.awayTeam, this.score);
+
+  Match.fromJson(Map<String, dynamic> json)
+      : homeTeam = json['homeTeam']['name'],
+        awayTeam = json['awayTeam']['name'],
+        score = json['score'];
+
+  String getDuration(){
+    return score['duration'];
+  }
+
+  List getHalfTimeScore(){
+    return _getScore('halfTime');
+  }
+
+  List getFullTimeScore(){
+    return _getScore('fullTime');
+  }
+
+  List getExtraTimeScore(){
+    return _getScore('extraTime');
+  }
+  
+  List getPenaltyScore(){
+    return _getScore('penalties');
+  }
+
+  List _getScore(String phase){
+    if (score != null) {
+      if (score.containsKey(phase)) {
+        return [score['phase']['homeTeam'], score[phase]['awayTeam']];
+      }
+    }
+    return null;
+  }
 }
 
 class MatchWidget extends StatelessWidget {
@@ -14,14 +47,51 @@ class MatchWidget extends StatelessWidget {
   final Match match;
   MatchWidget(this.match);
 
+  List<List<Widget>> buildScores(){
+    List<List<Widget>> result = [];
+    bool showHalfTime = true;
+    List score = match.getPenaltyScore();
+    if (score == null){
+      score = match.getExtraTimeScore();
+    }
+    if (score == null){
+      score = match.getFullTimeScore();
+    }
+    if (score == null){
+      showHalfTime = false;
+      score = match.getHalfTimeScore();
+    }
+    if (score != null){
+      result.add(_makeScoreWidget(score));
+    }
+    if (showHalfTime){
+      List halfTime = match.getHalfTimeScore();
+      if (halfTime != null){
+        result.add(_makeScoreWidget(halfTime));
+      }
+    }
+    return result;
+  }
+
+  List<Widget> _makeScoreWidget(List score){
+    return <Widget>[Text(score[0].toString()), Text(score[1].toString())];
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    var teams = [Text(match.homeTeam), Text(match.awayTeam)];
+    Row teamsRow = Row(children: teams);
+
+    List<Row> rows = [teamsRow];
+    buildScores().forEach( (List<Widget> scoreList) {
+      rows.add(Row(children: _makeScoreWidget(scoreList)));
+    });
+
     return Center(
-      child: Column(children: <Widget>[
-        Row(children: <Widget>[ Text(match.homeTeam), Text(match.awayTeam)]),
-        //Row(children: <Widget>[ Text(match.score.full["homeTeam"].toString()), Text(match.score.full["awayTeam"].toString())])
-      ])
+      child: Column(children: rows)
     );
   }
+
 
 }
--- lib/matches.dart
+++ lib/matches.dart
@@ -1,30 +1,122 @@
 import 'package:erling2020/match.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
 import 'home.dart';
 import 'match.dart';
 
 class MatchesPage extends HomePage {
+  @override
+  Widget buildBody(BuildContext context) {
+    return Matchlist();
+  }
+}
+
+class Matchlist extends StatefulWidget {
+  Matchlist({
+    Key key
+  }) : super(key: key);
+
+  final CollectionReference matchCollection = Firestore.instance.collection('matches');
 
   Widget _buildMatchItem(context, DocumentSnapshot document) {
-    return MatchWidget(Match(document.data['homeTeam']['name'], document.data['awayTeam']['name']));
+    return MatchWidget(Match.fromJson(document.data));
+  }
+
+  _MatchlistState createState() => _MatchlistState();
+}
+
+class _MatchlistState extends State<Matchlist> {
+
+  String competition = "BL1";
+  int matchday = 26;
+
+  @override
+  void initState() {
+    super.initState();
+    this.competition = "EC";
+  }
+
+  void setCompetition(String competition) async {
+    
+    DocumentSnapshot cdoc = await Firestore.instance.collection('competitions').document(competition).snapshots().first;
+    var currentMatchday = cdoc.data['currentMatchday'];
+
+    setState(() {
+      this.competition = competition;
+      matchday = currentMatchday;
+    });
+  }
+
+  void nextMatchDay(){
+    setState(() {
+      matchday++;
+    });
+  }
+
+  void previousMatchDay(){
+    setState(() {
+      matchday--;
+    });
+  }
+
+
+  Stream<QuerySnapshot> makeSnapshotStream(){
+    if (matchday != null) {
+      return widget.matchCollection.where('matchday', isEqualTo: matchday)
+          .snapshots();
+    } else {
+      DateTime now = DateTime.now();
+      DateTime _000 = now.subtract(Duration(hours: now.hour, minutes: now.minute, seconds: now.second, microseconds: now.microsecond, milliseconds: now.millisecond));
+      DateTime _001 = _000.add(Duration(days: 1));
+      return widget.matchCollection.where('utcDate', isGreaterThan: Timestamp.fromDate(_000)).where('ucDate', isLessThan: Timestamp.fromDate(_001)).snapshots();
+    }
+  }
+
+  Widget buildList(context, AsyncSnapshot snapshot){
+    return ListView.builder(
+        itemBuilder: (context, index) =>
+            widget._buildMatchItem(context, snapshot.data.documents[index]),
+        itemCount: snapshot.data.documents.length,
+        itemExtent: 80.0
+    );
   }
 
   @override
-  Widget buildBody(BuildContext context) {
+  Widget build(BuildContext context) {
     return StreamBuilder(
-        stream: Firestore.instance.collection('matches').where('status', isEqualTo: 'SCHEDULED').snapshots(),
+        stream: makeSnapshotStream(),
         builder: (context, snapshot) {
           if (!snapshot.hasData) return const Text("Loading...");
-          return ListView.builder(
-              itemBuilder: (context, index) =>
-                  _buildMatchItem(context, snapshot.data.documents[index]),
-              itemCount: snapshot.data.documents.length,
-              itemExtent: 80.0
-          );
+          if (matchday != null){
+            return Column(
+              children: <Widget>[
+                Text(matchday.toString() + ". Spieltag"),
+                Expanded(
+                  child: buildList(context, snapshot)
+                ),
+
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FlatButton(
+                      child: Text('Letzter Spieltag'),
+                      color: Colors.blue,
+                      onPressed: () => previousMatchDay()
+                    ),
+                    FlatButton(
+                      child: Text('Naechster Spieltag'),
+                      color: Colors.blue,
+                      onPressed: () => nextMatchDay()
+                    ),
+                  ],
+                )
+                ],
+            );
+          }
         }
     );
 
-    }
+  }
 }
