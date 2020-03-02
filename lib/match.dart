78e0b2f510fa3e44b3f7dc84a748fda4ff89a7fe
Christopher Prokop-Dorner <christopher.dorner@gmail.com>
2020-03-02 20:36:30 +0100

    nicer match widget layout with final score

--- lib/match.dart
+++ lib/match.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/cupertino.dart';
-
+import 'package:flutter/material.dart';
 
 class Match {
   String homeTeam, awayTeam;
@@ -12,27 +11,27 @@ class Match {
         awayTeam = json['awayTeam']['name'],
         score = json['score'];
 
-  String getDuration(){
+  String getDuration() {
     return score['duration'];
   }
 
-  List getHalfTimeScore(){
+  List getHalfTimeScore() {
     return _getScore('halfTime');
   }
 
-  List getFullTimeScore(){
+  List getFullTimeScore() {
     return _getScore('fullTime');
   }
 
-  List getExtraTimeScore(){
+  List getExtraTimeScore() {
     return _getScore('extraTime');
   }
-  
-  List getPenaltyScore(){
+
+  List getPenaltyScore() {
     return _getScore('penalties');
   }
 
-  List _getScore(String phase){
+  List _getScore(String phase) {
     if (score != null) {
       if (score.containsKey(phase)) {
         return [score['phase']['homeTeam'], score[phase]['awayTeam']];
@@ -43,55 +42,80 @@ class Match {
 }
 
 class MatchWidget extends StatelessWidget {
+  static const scoreTextStyle = TextStyle(
+    fontSize: 24,
+  );
 
   final Match match;
   MatchWidget(this.match);
 
-  List<List<Widget>> buildScores(){
+  List<List<Widget>> buildScores() {
     List<List<Widget>> result = [];
     bool showHalfTime = true;
     List score = match.getPenaltyScore();
-    if (score == null){
+    if (score == null) {
       score = match.getExtraTimeScore();
     }
-    if (score == null){
+    if (score == null) {
       score = match.getFullTimeScore();
     }
-    if (score == null){
+    if (score == null) {
       showHalfTime = false;
       score = match.getHalfTimeScore();
     }
-    if (score != null){
+    if (score != null) {
       result.add(_makeScoreWidget(score));
     }
-    if (showHalfTime){
+    if (showHalfTime) {
       List halfTime = match.getHalfTimeScore();
-      if (halfTime != null){
+      if (halfTime != null) {
         result.add(_makeScoreWidget(halfTime));
       }
     }
     return result;
   }
 
-  List<Widget> _makeScoreWidget(List score){
+  List<Widget> _makeScoreWidget(List score) {
     return <Widget>[Text(score[0].toString()), Text(score[1].toString())];
+  }
+
+  Widget _makeNiceScoreWidget(List score) {
+    return Container(
+      color: Colors.yellow,
+      alignment: Alignment.center,
+      child: Row(
+        children: <Widget>[
+          Text(score == null ? "x" : score[0], style: scoreTextStyle),
+          Text(":", style: scoreTextStyle),
+          Text(score == null ? "x" : score[1], style: scoreTextStyle)
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-
-    var teams = [Text(match.homeTeam), Text(match.awayTeam)];
-    Row teamsRow = Row(children: teams);
-
-    List<Row> rows = [teamsRow];
-    buildScores().forEach( (List<Widget> scoreList) {
-      rows.add(Row(children: _makeScoreWidget(scoreList)));
+    List<Row> scoreRows = [];
+    buildScores().forEach((List<Widget> scoreList) {
+      scoreRows.add(Row(children: _makeScoreWidget(scoreList)));
     });
 
-    return Center(
-      child: Column(children: rows)
-    );
+    return Card(
+        margin: EdgeInsets.all(4),
+        child: Column(children: <Widget>[
+          ListTile(
+              title: Row(
+                children: <Widget>[
+                  Expanded(flex: 4, child: Text(match.homeTeam)),
+                  Expanded(
+                      flex: 2,
+                      child: Row(children: <Widget>[
+                        _makeNiceScoreWidget(match.getFullTimeScore())
+                      ])),
+                  Expanded(flex: 4, child: Text(match.awayTeam))
+                ],
+              ),
+              subtitle: Column(children: scoreRows))
+        ]));
   }
-
-
 }
